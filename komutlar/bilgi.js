afa54d68e0fcbfb2f971bc1d02a22a3869afe76e
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-03-27 19:23:26 +0300

    Update bilgi.js

--- komutlar/bilgi.js
+++ komutlar/bilgi.js
@@ -3,19 +3,13 @@ const client = new Discord.Client();
 const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message) => {
-  if (message.channel.type !== 'dm') {
-    const ozelmesajkontrol = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('Özel mesajlarını kontrol et. :postbox:');
-    message.channel.sendEmbed(ozelmesajkontrol) }
 	const pingozel = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription('Bot sürümü: v' + ayarlar.surum + ' Yapımcı: Batu#2016 **Rahatsız Army**\n\n_**BOTU EKLEMEK İÇİN LİNK:**_\n\n[Tıkla Ve Davet Et](https://discordapp.com/oauth2/authorize?client_id=427079448953618432&scope=bot&permissions=2146958591)')
-};
+  };
+
 
 exports.conf = {
   enabled: true,
