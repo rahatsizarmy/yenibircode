d62365af1f249737238e1c533e72f3e90d5b311a
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-03-27 19:26:45 +0300

    Update bilgi.js

--- komutlar/bilgi.js
+++ komutlar/bilgi.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const ayarlar = require('../ayarlar.json');
 
 exports.run = (client, message) => {
 	const pingozel = new Discord.RichEmbed()
