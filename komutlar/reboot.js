b7590bad2b0b9cb4cb539b46a67a327c74f978d2
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-03-29 18:21:09 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,21 +1,17 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-
-    if (msg.author.id !== ayarlar.sahip) {
-      msg.reply('Benim yapımcım değilsin!');
-    } else {
-      msg.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
-      console.log(`BOT: Bot yeniden başlatılıyor...`);
-      process.exit(0);
-    })
-   }
-  }
+	
+    message.channel.send(`Bot yeniden başlatılıyor...`).then(msg => {
+    console.log(`BOT: Bot yeniden başlatılıyor...`);
+    process.exit(0);
+  })
+};
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['reset'],
+  aliases: [],
   permLevel: 4
 };
 
